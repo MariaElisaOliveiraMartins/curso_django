cd452745092ad2c0eaa692b77802c49635b79a66
mariaelisa <mariaelisaoliveiramartins@gmail.com>
2020-04-11 20:35:22 -0300

    Configurações do "Django Debug Toolbar" e do Backup no Heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -78,6 +78,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
 
+# Configuração do Django Debug Toolbar
+
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
